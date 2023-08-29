635fb1c8fc4f78f83910437c005bad8aed77b836
Jaemie Lee <jaelee90@gmail.com>
2023-08-29 00:11:45 +0000

    corrected staging models to pass successfully

--- models/staging/stg__merchants.sql
+++ models/staging/stg__merchants.sql
@@ -1,20 +1,16 @@
 with source_data as (
-select
-   *
-
-from {{ ref('merchants') }}
+    select *
+    from {{ ref('merchants') }}
 )
 
 select
-  shop_id,
-  merchant_created_at,
-  merchant_currency,
-  merchant_currency_symbol,
-  is_portal_active,
-  provider_id,
-  merchant_updated_at,
-  merchant_currency,
-  benchmark_vertical,
-  msm_market_segment,
-
+    shop_id
+  , merchant_created_at
+  , merchant_currency
+  , is_portal_active
+  , provider_id
+  , merchant_updated_at
+  , merchant_currency_symbol
+  , benchmark_vertical
+  , msm_market_segment
 from source_data
--- models/staging/stg__returns.sql
+++ models/staging/stg__returns.sql
@@ -1,26 +1,23 @@
 with source_data as (
-select
-   *
-
-from {{ ref('orders') }}
+    select *
+    from {{ ref('returns') }}
 )
 
 select
-   return_id
-  ,return_Created_at
-  ,destination_id
-  ,item_count
-  ,is_gift_return
-  ,is_in_store_return
-  ,order_id
-  ,provider_id
-  ,return_policy
-  ,return_processed_at
-  ,return_state
-  ,shop_id
-  ,refund_value_usd
-  ,exchange_value_usd
-  ,return_price_usd
-  ,total_shop_currency
-
+    return_id
+  , return_created_at
+  , destination_id
+  , item_count
+  , is_gift_return
+  , is_in_store_return
+  , order_id
+  , provider_id
+  , return_policy
+  , return_processed_at
+  , return_state
+  , shop_id
+  , refund_value_usd
+  , exchange_value_usd
+  , return_price_usd
+  , total_shop_currency
 from source_data
